7160f6d7fff0415bf86d5d1374d4eaaad63f6306
vanshsafe-site <vanshgargvl777@gmail.com>
2025-01-19 14:22:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ async function getAIResponse(message) {
         const response = await fetch('https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium', {
             method: 'POST',
             headers: {
-                'Authorization': 'Bearer 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',
+                'Authorization': 'Bearer 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',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
